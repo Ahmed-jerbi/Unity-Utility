40e666b88125a4f4e3f33536821c39a84a6239e1
Ahmed Jerbi <52385138+Ahmed-jerbi@users.noreply.github.com>
2020-09-01 14:59:17 +0200

    Initial commit

---
+++ CameraVehiculeController.cs
@@ -0,0 +1,35 @@
+﻿/*
+Camera Script for following a vehicule
+*/
+
+using UnityEngine;
+
+public class CameraVehiculeController : MonoBehaviour {
+    public Transform VehicleTransform;
+    public float DistanceFromVehicle;
+    public float HeightFromVehicle;
+    public float RotationDamping;
+    public float HeightDamping;
+
+    void LateUpdate()
+    {
+        var angle = VehicleTransform.eulerAngles.y;
+        var height = HeightFromVehicle + VehicleTransform.position.y;
+
+        var cameraAngle = transform.eulerAngles.y;
+        var cameraHeight = transform.position.y;
+
+        cameraAngle = Mathf.LerpAngle(cameraAngle, angle, RotationDamping * Time.deltaTime);
+        cameraHeight = Mathf.Lerp(cameraHeight, height, HeightDamping * Time.deltaTime);
+
+        var currentRotation = Quaternion.Euler(0, cameraAngle, 0);
+        transform.position = VehicleTransform.position;
+        transform.position -= currentRotation * Vector3.forward * DistanceFromVehicle;
+
+        var tmp = transform.position;
+        tmp.y = cameraHeight;
+
+        transform.position = tmp;
+        transform.LookAt(VehicleTransform);
+    }
+}
