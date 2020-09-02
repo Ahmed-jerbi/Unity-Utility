2d240b776155b0244aa042e4d1f0d40f2bfaf900
Ahmed Jerbi <52385138+Ahmed-jerbi@users.noreply.github.com>
2020-09-02 17:01:28 +0200

    Move object with arrows and mouse

--- SimpleMoveObject.cs
+++ SimpleMoveObject.cs
@@ -1,6 +1,6 @@
 /*
 
-Simple script for moving an object along the X (sides) and Z (forward) directions with the arrow keys 
+Simple script for moving an object along the X (sides) and Z (forward) directions with the arrow keys + Rotate with Mouse Axis
 - Horizontal  : X axis : left/right 
 - Vertical    : Z axis : forward/back
 
@@ -13,11 +13,16 @@ public class SimpleMoveObject : MonoBehaviour
 {
 
   public int moveSpeed=1;
+  public int rotSpeed=1;
 
   void Update()
   {
     // Translation along X and Z
     transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    
+    //Rotation around X and Y
+    transform.Rotate(rotSpeed * Input.GetAxis("Mouse Y"), rotSpeed * Input.GetAxis("Mouse X"), 0);
+    
   }
   
 }
